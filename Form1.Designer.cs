480d5a25f09283b82dd5628e9c05e0b3d36fe120
Sh1r0ko11 <143630515+Sh1r0ko11@users.noreply.github.com>
2025-05-04 22:55:28 +0200

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,131 @@
+﻿namespace Installer_multi_tool
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            label1 = new Label();
+            linkLabel1 = new LinkLabel();
+            button1 = new Button();
+            linkLabel2 = new LinkLabel();
+            downloadProgressBar = new ProgressBar();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.BackColor = Color.Transparent;
+            label1.Font = new Font("Showcard Gothic", 48F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label1.Location = new Point(156, -2);
+            label1.Name = "label1";
+            label1.Size = new Size(353, 98);
+            label1.TabIndex = 0;
+            label1.Text = "EXtool";
+            label1.Click += label1_Click;
+            // 
+            // linkLabel1
+            // 
+            linkLabel1.AutoSize = true;
+            linkLabel1.BackColor = Color.Transparent;
+            linkLabel1.Font = new Font("Snap ITC", 18F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            linkLabel1.LinkColor = Color.BlanchedAlmond;
+            linkLabel1.Location = new Point(466, 86);
+            linkLabel1.Name = "linkLabel1";
+            linkLabel1.Size = new Size(177, 39);
+            linkLabel1.TabIndex = 1;
+            linkLabel1.TabStop = true;
+            linkLabel1.Text = "Installer";
+            linkLabel1.VisitedLinkColor = Color.Red;
+            // 
+            // button1
+            // 
+            button1.BackColor = Color.White;
+            button1.Font = new Font("Showcard Gothic", 24F, FontStyle.Bold | FontStyle.Italic, GraphicsUnit.Point, 0);
+            button1.ForeColor = Color.Black;
+            button1.Image = (Image)resources.GetObject("button1.Image");
+            button1.Location = new Point(47, 209);
+            button1.Name = "button1";
+            button1.Size = new Size(596, 186);
+            button1.TabIndex = 2;
+            button1.Text = "Install";
+            button1.UseVisualStyleBackColor = false;
+            // 
+            // linkLabel2
+            // 
+            linkLabel2.AutoSize = true;
+            linkLabel2.BackColor = Color.Transparent;
+            linkLabel2.Font = new Font("Snap ITC", 18F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            linkLabel2.LinkColor = Color.BlanchedAlmond;
+            linkLabel2.Location = new Point(29, 86);
+            linkLabel2.Name = "linkLabel2";
+            linkLabel2.RightToLeft = RightToLeft.Yes;
+            linkLabel2.Size = new Size(177, 39);
+            linkLabel2.TabIndex = 3;
+            linkLabel2.TabStop = true;
+            linkLabel2.Text = "Installer";
+            linkLabel2.VisitedLinkColor = Color.Red;
+            // 
+            // downloadProgressBar
+            // 
+            downloadProgressBar.AccessibleDescription = "downloadProgressBar";
+            downloadProgressBar.AccessibleName = "downloadProgressBar";
+            downloadProgressBar.BackColor = Color.MediumTurquoise;
+            downloadProgressBar.ForeColor = Color.Red;
+            downloadProgressBar.Location = new Point(212, 86);
+            downloadProgressBar.Name = "downloadProgressBar";
+            downloadProgressBar.Size = new Size(226, 42);
+            downloadProgressBar.TabIndex = 4;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.White;
+            ClientSize = new Size(697, 395);
+            Controls.Add(downloadProgressBar);
+            Controls.Add(linkLabel2);
+            Controls.Add(button1);
+            Controls.Add(linkLabel1);
+            Controls.Add(label1);
+            Icon = (Icon)resources.GetObject("$this.Icon");
+            Name = "Form1";
+            Text = "Youre Installer name";
+            Load += Form1_Load;
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label label1;
+        private LinkLabel linkLabel1;
+        private Button button1;
+        private LinkLabel linkLabel2;
+        private ProgressBar downloadProgressBar;
+    }
+}
---
+++ Form1.cs
@@ -0,0 +1,182 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using System.Net;
+using System.Runtime.InteropServices;
+using System.Windows.Forms;
+
+namespace Installer_multi_tool
+{
+    public partial class Form1 : Form
+    {
+        //Левый
+        private string downloadUrl = "https://github.com/Sh1r0ko11/IP-Beast/releases/download/Beta/beast.Tool.Beta.1.ONLY.open.source.zip";
+        private string redirectUrl = "https://github.com/Sh1r0ko11";
+
+       
+        private bool isMouseDown = false;
+        private Point mouseOffset;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            
+            this.FormBorderStyle = FormBorderStyle.Sizable;
+            this.Opacity = 0.95; 
+            this.BackColor = System.Drawing.Color.White; 
+            this.DoubleBuffered = true;
+            this.MaximizeBox = true;
+            this.MinimizeBox = true;
+            this.ControlBox = true;
+            this.ShowInTaskbar = true;
+
+            
+            this.TransparencyKey = System.Drawing.Color.White;  
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            EnableBlur(); 
+
+            
+            foreach (Control control in this.Controls)
+            {
+                if (control is Button button)
+                {
+                    button.Click += Button_Click;
+                }
+                else if (control is LinkLabel linkLabel)
+                {
+                    linkLabel.LinkClicked += LinkLabel_LinkClicked;
+                }
+            }
+
+            
+            this.MouseDown += Form1_MouseDown;
+            this.MouseMove += Form1_MouseMove;
+            this.MouseUp += Form1_MouseUp;
+
+            downloadProgressBar.Value = 0;
+        }
+
+        private void Button_Click(object sender, EventArgs e)
+        {
+            string downloadsPath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.UserProfile), "Downloads");
+            string fileName = Path.GetFileName(new Uri(downloadUrl).AbsolutePath);
+            string destinationPath = Path.Combine(downloadsPath, fileName);
+
+            WebClient client = new WebClient();
+
+            client.DownloadProgressChanged += (s, args) =>
+            {
+                downloadProgressBar.Value = args.ProgressPercentage;
+            };
+
+            client.DownloadFileCompleted += (s, args) =>
+            {
+                MessageBox.Show($"Download completed:\n{destinationPath}", "Done", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                downloadProgressBar.Value = 0;
+            };
+
+            try
+            {
+                client.DownloadFileAsync(new Uri(downloadUrl), destinationPath);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Download failed:\n{ex.Message}", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void LinkLabel_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            Process.Start(new ProcessStartInfo
+            {
+                FileName = redirectUrl,
+                UseShellExecute = true
+            });
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+       
+        private void EnableBlur()
+        {
+            var accent = new AccentPolicy();
+            accent.AccentState = AccentState.ACCENT_ENABLE_ACRYLICBLURBEHIND;
+
+            
+            accent.GradientColor = unchecked((int)0x99A0A0A0); 
+
+            int size = Marshal.SizeOf(accent);
+            IntPtr accentPtr = Marshal.AllocHGlobal(size);
+            Marshal.StructureToPtr(accent, accentPtr, false);
+
+            var data = new WindowCompositionAttributeData();
+            data.Attribute = 19;
+            data.SizeOfData = size;
+            data.Data = accentPtr;
+
+            NativeMethods.SetWindowCompositionAttribute(this.Handle, ref data);
+
+            Marshal.FreeHGlobal(accentPtr);
+        }
+
+        
+        enum AccentState
+        {
+            ACCENT_DISABLED = 0,
+            ACCENT_ENABLE_GRADIENT = 1,
+            ACCENT_ENABLE_TRANSPARENTGRADIENT = 2,
+            ACCENT_ENABLE_BLURBEHIND = 3,
+            ACCENT_ENABLE_ACRYLICBLURBEHIND = 4,
+            ACCENT_ENABLE_HOSTBACKDROP = 5,
+            ACCENT_INVALID_STATE = 6
+        }
+
+        struct AccentPolicy
+        {
+            public AccentState AccentState;
+            public int AccentFlags;
+            public int GradientColor;
+            public int AnimationId;
+        }
+
+        struct WindowCompositionAttributeData
+        {
+            public int Attribute;
+            public IntPtr Data;
+            public int SizeOfData;
+        }
+
+        class NativeMethods
+        {
+            [DllImport("user32.dll")]
+            public static extern int SetWindowCompositionAttribute(IntPtr hwnd, ref WindowCompositionAttributeData data);
+        }
+
+        // To allow dragging the form
+        private void Form1_MouseDown(object sender, MouseEventArgs e)
+        {
+            isMouseDown = true;
+            mouseOffset = new Point(e.X, e.Y);
+        }
+
+        private void Form1_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (isMouseDown)
+            {
+                this.Location = new Point(this.Left + e.X - mouseOffset.X, this.Top + e.Y - mouseOffset.Y);
+            }
+        }
+
+        private void Form1_MouseUp(object sender, MouseEventArgs e)
+        {
+            isMouseDown = false;
+        }
+    }
+}
